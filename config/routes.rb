7cefc996ff92af359ad39a5fdfab030aee4d611b
Tony <tonyblum@me.com>
2016-05-17 17:39:42 -0700

    create channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'rooms#show'
 
+  
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
